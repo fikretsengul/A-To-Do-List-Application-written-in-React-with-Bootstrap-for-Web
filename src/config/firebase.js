8f3bac4c3cf04276644a655cfacde5982c4a8d41
Fikret Şengül <fikretsengul@users.noreply.github.com>
2020-07-17 00:42:02 +0300

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 export const config = {
-    apiKey: "AIzaSyBfMXry1SsaodPsMA70779ARk2AT-zOSxo",
-    authDomain: "todoapp-abe0a.firebaseapp.com",
-    databaseURL: "https://todoapp-abe0a.firebaseio.com",
-    projectId: "todoapp-abe0a",
-    storageBucket: "todoapp-abe0a.appspot.com",
-    messagingSenderId: "78864891604",
-    appId: "1:78864891604:web:7c15919bad5ad815"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 firebase.initializeApp(config)
